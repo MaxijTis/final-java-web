d531bb022a9da486d8943436ebcdc125edf1db6d
Nicolas Ortiz <nicoortiz9870@gmail.com>
2024-07-13 15:11:08 -0300

    Inicio de sesión y registro front

--- BackEnd/src/main/java/com/codoacodo/controller/ProductController.java
+++ BackEnd/src/main/java/com/codoacodo/controller/ProductController.java
@@ -12,6 +12,7 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/products")
+@CrossOrigin
 public class ProductController {
 
     private final ProductService productService;
